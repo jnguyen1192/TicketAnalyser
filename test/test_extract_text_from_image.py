a8070029f47d7eebbc241cf642409acf3a36f13d
Johnny Nguyen <johdu13@hotmail.fr>
2020-12-30 23:39:17 +0100

    First try to use Google Cloud Vision

--- test/test_extract_text_from_image.py
+++ test/test_extract_text_from_image.py
@@ -125,6 +125,110 @@ class MyTestCase(unittest.TestCase):
         for path_img in path_img_list:
             ticketanalyser.ticket_image_to_text(path_img)
 
+    def test_rotate(self):
+        # import the necessary packages
+        import numpy as np
+        import argparse
+        import imutils
+        import cv2
+
+        # Read the image and create a blank mask
+        image  = cv2.imread("../data/Aldi_1.jpg")
+        grayScale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Find edges in the image using canny edge detection method
+        # Calculate lower threshold and upper threshold using sigma = 0.33
+        sigma = 0.33
+        v = np.median(grayScale)
+        low = int(max(0, (1.0 - sigma) * v))
+        high = int(min(255, (1.0 + sigma) * v))
+
+        edged = cv2.Canny(grayScale, low, high)
+
+        # After finding edges we have to find contours
+        # Contour is a curve of points with no gaps in the curve
+        # It will help us to find location of shapes
+
+        # cv2.RETR_EXTERNAL is passed to find the outermost contours (because we want to outline the shapes)
+        # cv2.CHAIN_APPROX_SIMPLE is removing redundant points along a line
+        (cnts, _) = cv2.findContours(edged,
+                                        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        '''
+        We are going to use contour approximation method to find vertices of
+        geometric shapes. The alogrithm  is also known as Ramer Douglas Peucker alogrithm.
+        In OpenCV it is implemented in cv2.approxPolyDP method.abs
+        detectShape() function below takes a contour as parameter and
+        then returns its shape
+         '''
+
+        def detectShape(cnt):
+            shape = 'unknown'
+            # calculate perimeter using
+            peri = cv2.arcLength(c, True)
+            # apply contour approximation and store the result in vertices
+            vertices = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+            # If the shape it triangle, it will have 3 vertices
+            if len(vertices) == 3:
+                shape = 'triangle'
+
+            # if the shape has 4 vertices, it is either a square or
+            # a rectangle
+            elif len(vertices) == 4:
+                # using the boundingRect method calculate the width and height
+                # of enclosing rectange and then calculte aspect ratio
+
+                x, y, width, height = cv2.boundingRect(vertices)
+                aspectRatio = float(width) / height
+
+                # a square will have an aspect ratio that is approximately
+                # equal to one, otherwise, the shape is a rectangle
+                if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+                    shape = "square"
+                else:
+                    shape = "rectangle"
+
+            # if the shape is a pentagon, it will have 5 vertices
+            elif len(vertices) == 5:
+                shape = "pentagon"
+
+            # otherwise, we assume the shape is a circle
+            else:
+                shape = "circle"
+
+            # return the name of the shape
+            return shape
+
+        # Now we will loop over every contour
+        # call detectShape() for it and
+        # write the name of shape in the center of image
+
+        # loop over the contours
+        for c in cnts:
+            # compute the moment of contour
+            M = cv2.moments(c)
+            # From moment we can calculte area, centroid etc
+            # The center or centroid can be calculated as follows
+            cX = int(M['m10'] / M['m00'])
+            cY = int(M['m01'] / M['m00'])
+
+            # call detectShape for contour c
+            shape = detectShape(c)
+
+            # Outline the contours
+            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+
+            # Write the name of shape on the center of shapes
+            cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (255, 255, 255), 2)
+
+            imS = cv2.resize(image, (960, 540))  # Resize image
+            cv2.imshow("output", imS)  # Show image
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 
 
     def test_extract_text_from_image_6(self):
